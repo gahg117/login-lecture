5ac96d70ac10b0c9b7fd0af625c91ee03d08f4c3
unknown <ga117@naver.com>
2023-02-23 13:42:58 +0900

    노트북 테스트

---
+++ app/src/routes/home/home.ctrl.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const { use } = require(".");
+
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  },
+};
+
+const users = {
+  id: ["ga117", "gahg117", "이가형"],
+  psword: ["dash117", "dash117@", "이가형@"],
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
+  },
+};
+
+module.exports = {
+  output,
+  process,
+};
