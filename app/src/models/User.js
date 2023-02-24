3993bb0e4c2f90a3679174c3541eb5a4fb6e2a91
unknown <ga117@naver.com>
2023-02-24 13:49:59 +0900

    로그인 꾸미기

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,15 +9,14 @@ class User {
 
   login() {
     const body = this.body;
-    console.log(body.id);
     const { id, psword } = UserStorage.getUserInfo(body.id);
     if (id) {
       if (id === body.id && psword === body.psword) {
-        return { succes: true };
+        return { success: true };
       }
-      return { succes: false, msg: "비밀번호가 틀렸습니다." };
+      return { success: false, msg: "비밀번호가 틀렸습니다." };
     }
-    return { succes: false, msg: "존재하지 않는 아이디입니다." };
+    return { success: false, msg: "존재하지 않는 아이디입니다." };
   }
 }
 
