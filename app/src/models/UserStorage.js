d180a2989c18e76cd2193dc94fa02f85494886cc
unknown <ga117@naver.com>
2023-02-24 14:31:03 +0900

    회원가입 라우팅 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -8,16 +8,16 @@ class UserStorage {
     name: ["가", "가형", "이가형"],
   };
 
-  static getUsers(...fields) {
-    const users = this.#user;
-    const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {
-        newUsers[field] = users[field];
-      }
-      return newUsers;
-    }, {});
-    return newUsers;
-  }
+  // static getUsers(...fields) {
+  //   const users = this.#user;
+  //   const newUsers = fields.reduce((newUsers, field) => {
+  //     if (users.hasOwnProperty(field)) {
+  //       newUsers[field] = users[field];
+  //     }
+  //     return newUsers;
+  //   }, {});
+  //   return newUsers;
+  // }
 
   static getUserInfo(id) {
     const users = this.#user;
@@ -28,6 +28,15 @@ class UserStorage {
       return newUser;
     }, {});
     return userInfo;
+  }
+
+  static save(userInfo) {
+    const users = this.#user;
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.psword.push(userInfo.psword);
+    console.log(users);
+    return { success: true };
   }
 }
 
